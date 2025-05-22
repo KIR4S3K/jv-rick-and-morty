283832f9d703895df5cc6292a5af39d49f4a8461
Mateusz <mateusz.kiryk@o2.pl>
2025-05-22 12:53:50 +0200

    Third solution

--- src/main/java/mate/academy/rickandmorty/service/CharacterService.java
+++ src/main/java/mate/academy/rickandmorty/service/CharacterService.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import java.util.stream.Collectors;
 import mate.academy.rickandmorty.dto.CharacterDto;
 import mate.academy.rickandmorty.entity.CharacterEntity;
+import mate.academy.rickandmorty.exception.NoCharactersFoundException;
 import mate.academy.rickandmorty.mapper.CharacterMapper;
 import mate.academy.rickandmorty.repository.CharacterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,9 @@ public class CharacterService {
 
     public CharacterDto getRandomCharacter() {
         List<CharacterEntity> all = repository.findAll();
+        if (all.isEmpty()) {
+            throw new NoCharactersFoundException("No characters found in the database.");
+        }
         CharacterEntity picked = all.get(random.nextInt(all.size()));
         return CharacterMapper.toDto(picked);
     }
